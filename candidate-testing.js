f18175a137524c247a04ffcc0f43a71c39ea9ca7
jacxd64 <70988180+jacxd64@users.noreply.github.com>
2024-04-01 16:23:09 -0500

    graded assignment 1, part 2, done for real this time

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,7 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -23,7 +23,7 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question)
+  //candidateAnswer = input.question(question)
   for (let i = 0; i < questions.length; i++) {
     candidateAnswers.push(input.question(questions[i]))
   }
